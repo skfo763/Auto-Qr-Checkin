b392956b564a541dff09264bfe3a2e74aca5b23b
Changyeon-Seo <scy7351@naver.com>
2020-10-14 10:28:21 +0900

    revise keystore

---
+++ buildSrc/build/generated-sources/kotlin-dsl-plugins/kotlin/SigningConfigPlugin.kt
@@ -0,0 +1,17 @@
+/**
+ * Precompiled [signing-config.gradle.kts][Signing_config_gradle] script plugin.
+ *
+ * @see Signing_config_gradle
+ */
+class SigningConfigPlugin : org.gradle.api.Plugin<org.gradle.api.Project> {
+    override fun apply(target: org.gradle.api.Project) {
+        try {
+            Class
+                .forName("Signing_config_gradle")
+                .getDeclaredConstructor(org.gradle.api.Project::class.java, org.gradle.api.Project::class.java)
+                .newInstance(target, target)
+        } catch (e: java.lang.reflect.InvocationTargetException) {
+            throw e.targetException
+        }
+    }
+}
