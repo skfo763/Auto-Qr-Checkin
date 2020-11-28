ed7a6586a85408d0c9f7c37d8518dacaa81a938b
Changyeon-Seo <scy7351@naver.com>
2020-11-28 12:22:30 +0900

    gradle 버전 업그레이드

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ ext {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.0.1")
+    implementation("com.android.tools.build:gradle:4.0.2")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
     implementation(kotlin("gradle-plugin", "1.4.0"))
 }
