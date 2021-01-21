1cdd97bc335da5e3ef32ccf533632caba1e0b37f
Changyeon-Seo <scy7351@naver.com>
2021-01-21 23:14:05 +0900

    버전 범프

--- buildSrc/src/main/kotlin/com/skfo763/gradle/global/AppBase.kt
+++ buildSrc/src/main/kotlin/com/skfo763/gradle/global/AppBase.kt
@@ -8,6 +8,6 @@ object Sdk {
 
 object AppVersion {
     const val applicationId = "com.skfo763.qrcheckin"
-    const val versionCode = 105001
-    const val versionName = "1.5.1"
+    const val versionCode = 106000
+    const val versionName = "1.6.0"
 }
